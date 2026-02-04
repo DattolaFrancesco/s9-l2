ff5f3975d32ed80dd117a766e5526bd45af1c952
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-02-04 16:36:22 +0100

    .

--- src/components/singleBook.jsx
+++ src/components/singleBook.jsx
@@ -3,22 +3,44 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Component } from "react";
 
-function SingleBook(props) {
-  const test = props.test.title.slice(0, 13);
-  return (
-    <Col xs={6} sm={6} md={4} lg={3} className={props.display}>
-      <Card>
-        <div>
-          <Card.Img variant="top" src={props.test.img} style={{ maxHeight: "15rem", objectFit: "cover" }} />
-        </div>
-        <Card.Body>
-          <Card.Title>{test}...</Card.Title>
-          <Card.Text>{props.test.price}</Card.Text>
-          <Button variant="primary">BUY</Button>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+
+  render() {
+    const { test, display } = this.props;
+    const titleShort = test.title.slice(0, 13);
+    return (
+      <Col xs={6} sm={6} md={4} lg={3} className={display}>
+        <Card>
+          <div>
+            <Card.Img
+              onClick={() => {
+                if (!this.state.selected) {
+                  this.setState({
+                    selected: true,
+                  });
+                } else {
+                  this.setState({
+                    selected: false,
+                  });
+                }
+              }}
+              variant="top"
+              src={test.img}
+              style={{ maxHeight: "15rem", objectFit: "cover" }}
+            />
+          </div>
+          <Card.Body>
+            <Card.Title>{titleShort}...</Card.Title>
+            <Card.Text>{test.price}</Card.Text>
+            <Button variant="primary">BUY</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  }
 }
 
 export default SingleBook;
